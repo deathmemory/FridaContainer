1843e5a6ad3ac516d4e326661f2826cc4be5ffbb
deathmemory <deathmemory@163.com>
2020-09-16 13:08:33 +0800

    加入 RandomStringUtils 字符串随机生成类

---
+++ utils/common/RandomStringUtils.ts
@@ -0,0 +1,22 @@
+/**
+ * @author: xingjun.xyf
+ * @contact: deathmemory@163.com
+ * @file: RandomStringUtils.js
+ * @time: 2020/9/15 4:16 PM
+ * @desc:
+ */
+
+export class RandomStringUtils {
+
+    static randomHexStr(count: number) {
+        return this.random(count, "1234567890ABCDEF");
+    }
+
+    static random(count: number, basestr: string) {
+        var res = "";
+        for (var i = 0; i < count; i++) {
+            res += basestr.charAt(Math.floor(Math.random() * basestr.length));
+        }
+        return res;
+    }
+}
