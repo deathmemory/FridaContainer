205c71e3e64062661c695abfd9c4cd66b169c1ba
deathmemory <deathmemory@163.com>
2020-10-09 10:38:09 +0800

    add int2ip, ip2int

--- utils/common/StringUtils.ts
+++ utils/common/StringUtils.ts
@@ -19,4 +19,22 @@ export class StringUtils {
         }
         return res;
     }
+
+    static int2ip (ipInt: number, isHighEndian: boolean) {
+        if (isHighEndian) {
+            return ( (ipInt>>>24) +'.' + (ipInt>>16 & 255) +'.' + (ipInt>>8 & 255) +'.' + (ipInt & 255) );
+        }
+        else {
+            return ( (ipInt & 255) + '.' + (ipInt>>8 & 255) + '.' + (ipInt>>16 & 255) + '.' + (ipInt>>>24));
+        }
+    }
+
+    static ip2int(ip: string, isHighEndian: boolean) {
+        if (isHighEndian) {
+            return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+        }
+        else {
+            return ip.split('.').reduce(function(ipInt, octet, idx) { return (parseInt(octet, 10) << idx * 8) + ipInt}, 0) >>> 0;
+        }
+    }
 }
