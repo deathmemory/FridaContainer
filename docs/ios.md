f1cf3efb858c5030dc045be1a2906787eb076b8d
deathmemory <deathmemory@163.com>
2020-10-13 16:19:07 +0800

    ios 增加 dump_ui，trace_url，trace_NSLog

--- docs/ios.md
+++ docs/ios.md
@@ -9,5 +9,20 @@ const addr = FCiOS.iosOpts.getFuncAddr('*[NVEnvironment deviceId]');
 ## 打印堆栈
 
 ```typescript
-FCiOS.showStacks();
+FCiOS.iosOpts.showStacks(this);
+```
+
+## dump ui
+```typescript
+console.log(FCiOS.iosOpts.dump_ui());
+```
+
+## trace openURL
+```typescript
+FCiOS.iosOpts.trace_url();
+```
+
+## trace NSLog
+```typescript
+FCiOS.iosOpts.trace_NSLog();
 ```
--- utils/ios/iosOpts.ts
+++ utils/ios/iosOpts.ts
@@ -1,8 +1,9 @@
+///<reference path="../../node_modules/@types/frida-gum/index.d.ts"/>
 import {DMLog} from "../dmlog";
 
 export class IosOpts {
 // generic getFuncAddr
-    static getFuncAddr(pattern: string) : NativePointer {
+    static getFuncAddr(pattern: string): NativePointer {
         var tag = 'getFuncAddr';
         var type: ApiResolverType = (pattern.indexOf(" ") === -1) ? "module" : "objc";
         DMLog.i(tag, 'getFuncAddr type: ' + type);
@@ -36,5 +37,64 @@ export class IosOpts {
         DMLog.i('showStacks', '\tBacktrace:\n\t' + Thread.backtrace(thiz.context,
             Backtracer.ACCURATE).map(DebugSymbol.fromAddress)
             .join('\n\t'));
+    }
+
+    static dump_ui() {
+        try {
+            var current_window = ObjC.classes.UIWindow.keyWindow();
+            return current_window.recursiveDescription().toString();
+        }
+        catch (e) {
+            return e;
+        }
+
+    }
+
+    /**
+     * trace openURL
+     */
+    static trace_url() {
+        //Twitter: https://twitter.com/xploresec
+        //GitHub: https://github.com/interference-security
+        // Get a reference to the openURL selector
+        var openURL = ObjC.classes.UIApplication["- openURL:"];
+
+        // Intercept the method
+        Interceptor.attach(openURL.implementation, {
+            onEnter: function (args) {
+                // As this is an ObjectiveC method, the arguments are as follows:
+                // 0. 'self'
+                // 1. The selector (openURL:)
+                // 2. The first argument to the openURL selector
+                var myNSURL = new ObjC.Object(args[2]);
+                // Convert it to a JS string
+                var myJSURL = myNSURL.absoluteString().toString();
+                // Log it
+                DMLog.i('openURL', "Launching URL: " + myJSURL);
+                //send(myJSURL);
+            }
+        });
+
+    }
+
+    static trace_NSLog() {
+        const NSLog_ptr = Module.findExportByName("Foundation", "NSLog");
+        if (NSLog_ptr) {
+            Interceptor.attach(NSLog_ptr, {
+                onEnter: function (args) {
+                    DMLog.i('NSLog', new ObjC.Object(args[0]).toString());
+                }
+            });
+        }
+
+        const NSLogv_ptr = Module.findExportByName("Foundation", "NSLogv");
+        if (NSLogv_ptr) {
+            Interceptor.attach(NSLogv_ptr, {
+                onEnter: function (args) {
+                    DMLog.i('NSLogv', new ObjC.Object(args[0]).toString());
+                }
+            });
+        }
+
     }
 }
