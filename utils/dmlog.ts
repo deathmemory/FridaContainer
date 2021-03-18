e53f76c22d62e79499343ad2415984bb6574a1e8
deathmemory <deathmemory@163.com>
2021-03-18 19:22:36 +0800

    log 样式调整加入 threadid

--- utils/dmlog.ts
+++ utils/dmlog.ts
@@ -3,20 +3,24 @@ export class DMLog {
 
     static d(tag: string, str: string) {
         if (this.bDebug) {
-            DMLog.log_('DEBUG', tag, str);
+            DMLog.log_(console.log, 'DEBUG', tag, str);
         }
     }
 
     static i(tag: string, str: string) {
-        DMLog.log_('INFO', tag, str);
+        DMLog.log_(console.log, 'INFO', tag, str);
+    }
+
+    static w(tag: string, str: string) {
+        DMLog.log_(console.warn, 'WARN', tag, str);
     }
 
     static e(tag: string, str: string) {
-        DMLog.log_('ERROR', tag, str);
+        DMLog.log_(console.error, 'ERROR', tag, str);
     }
 
-    static log_(leval: string, tag: string, str: string) {
-        console.log('[' + leval + '][' + new Date().toLocaleString('zh-CN') + '][' + tag + ']: ' + str);
+    static log_(logfunc: (message?: any, ...optionalParams: any[]) => void , leval: string, tag: string, str: string) {
+        logfunc(`[${leval}][${new Date().toLocaleString('zh-CN')}][${Process.getCurrentThreadId()}][${tag}]: ${str}`);
     }
 
     static send(tag: string, content: string) {
